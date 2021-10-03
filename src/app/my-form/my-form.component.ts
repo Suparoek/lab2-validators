45be90ed5f96c8e087c306f7cc3eeaa75e0e5a4e
Suparoek <39317424+Suparoek@users.noreply.github.com>
2021-10-03 21:10:23 +0700

    day4lab1

---
+++ src/app/my-form/my-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup ,FormBuilder} from '@angular/forms';
+
+@Component({
+  selector: 'app-my-form',
+  templateUrl: './my-form.component.html',
+  styleUrls: ['./my-form.component.css']
+})
+export class MyFormComponent implements OnInit {
+  form!:FormGroup;
+  constructor(private fb: FormBuilder) { 
+    this.form = this.fb.group({
+      PhoneNo: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
